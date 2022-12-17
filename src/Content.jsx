4d5daa395204aedfd30c9eb62169345155eebc95
Faran Saeed <faransaeed@rocketmail.com>
2022-12-17 13:41:28 +0500

    Done part-1a exercises

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,11 +1,11 @@
 import Part from './Part';
 
-const Content = ({ parts, exercises }) => {
+const Content = ({ parts }) => {
   return (
     <>
-      <Part part={parts.part1} exercise={exercises.exercises1} />
-      <Part part={parts.part1} exercise={exercises.exercises1} />
-      <Part part={parts.part1} exercise={exercises.exercises1} />
+      <Part part={parts[0].name} exercise={parts[0].exercises} />
+      <Part part={parts[1].name} exercise={parts[1].exercises} />
+      <Part part={parts[2].name} exercise={parts[2].exercises} />
     </>
   );
 };
