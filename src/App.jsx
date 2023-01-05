3cc272252bb627707c5729eec1c9127798abf1c9
Faran Saeed <faransaeed@rocketmail.com>
2023-01-05 17:47:27 +0500

    Completed part-2b_2.11

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 // React
-import { useState } from 'react';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 import Filtered from './components/Filtered';
 import Form from './components/Form';
 
@@ -7,14 +8,14 @@ import Form from './components/Form';
 import Search from './components/Search';
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
-  ]);
-
+  const [persons, setPersons] = useState([]);
   const [filtered, setFiltered] = useState('');
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3000/persons')
+      .then((resp) => setPersons(resp.data));
+  }, []);
 
   return (
     <div>
