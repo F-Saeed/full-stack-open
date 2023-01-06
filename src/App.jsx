d24a4aed79c509170abb2aff5cea83c3b1d72ad9
Faran Saeed <faransaeed@rocketmail.com>
2023-01-06 11:49:40 +0500

    Completed part-2b_2.12

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,50 @@
 // React
 import axios from 'axios';
-import { useState, useEffect } from 'react';
-import Filtered from './components/Filtered';
-import Form from './components/Form';
-
-// Components
+import { useState, useEffect, Fragment } from 'react';
 import Search from './components/Search';
 
 const App = () => {
-  const [persons, setPersons] = useState([]);
-  const [filtered, setFiltered] = useState('');
+  const [countries, setCountries] = useState([]);
+  const [search, setSearch] = useState('');
 
   useEffect(() => {
-    axios
-      .get('http://localhost:3000/persons')
-      .then((resp) => setPersons(resp.data));
-  }, []);
+    if (search) {
+      axios
+        .get(`https://restcountries.com/v3.1/name/${search}`)
+        .then((resp) => setCountries(resp.data));
+    }
+  }, [search]);
 
   return (
     <div>
-      <h2>Phonebook</h2>
-      <Search persons={persons} setFiltered={setFiltered} />
-      <br />
-      <Form
-        persons={persons}
-        setPersons={setPersons}
-        setFiltered={setFiltered}
-      />
-      <h2>Numbers</h2>
-      <Filtered filtered={filtered} persons={persons} />
+      {' '}
+      <p>
+        Find Country <Search search={search} setSearch={setSearch} />
+      </p>
+      {countries.length > 10 ? (
+        'Too Many Countries'
+      ) : countries.length === 1 ? (
+        countries.map((item) => (
+          <Fragment key={item.cca2}>
+            <h2>{item.name.common}</h2>
+            <p>capital {item.capital[0]}</p>
+            <p>area {item.area}</p>
+            <h4>Languages</h4>
+            <ul>
+              {Object.values(item.languages).map((lang) => (
+                <li key={lang}>{lang}</li>
+              ))}
+            </ul>
+            <img src={item.flags.png} alt='Country Flag' />
+          </Fragment>
+        ))
+      ) : (
+        <ul>
+          {countries.map((item) => (
+            <li key={item.cca2}>{item.name.common}</li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,36 +1,10 @@
-import { useState } from 'react';
-
-const Search = ({ persons, setFiltered }) => {
-  const [search, setSearch] = useState('');
-
-  const searchName = (event) => {
-    event.preventDefault();
-
-    const searchItem = event.target.children[0].value.toLowerCase();
-    const filteredNames = persons.filter(
-      (item) => item.name.toLowerCase() === searchItem
-    );
-
-    setFiltered(filteredNames);
-    setSearch('');
-  };
-
+const Search = ({ search, setSearch }) => {
   return (
-    <div>
-      Filter Names:
-      <form onSubmit={searchName}>
-        <input
-          type='text'
-          minLength='1'
-          value={search}
-          onChange={(event) => setSearch(event.target.value)}
-        />
-        <button type='submit'>Search Name</button>
-      </form>
-      <button type='button' onClick={() => setFiltered('')}>
-        Reset
-      </button>
-    </div>
+    <input
+      type='text'
+      onChange={(event) => setSearch(event.target.value)}
+      value={search}
+    />
   );
 };
 
